6dd3c685b558616d11595b7b683625b4d54d7fbe
Marcin Kuryło <mkurylo@catalogicsoftware.com>
2022-02-08 21:02:03 +0100

    feat/create-hover-function-skeleton

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,9 @@
 import * as vscode from 'vscode';
 import { Commands } from './commands';
+import { FileSystemManager } from './utils/fileSytemManager';
 
 export const activate = async (context: vscode.ExtensionContext) => {
+	const cache = await FileSystemManager.fetchJson();
 	const commands = [
 		Commands.registerSetLanguage(),
 		Commands.registerSetPath(),
@@ -10,7 +12,28 @@ export const activate = async (context: vscode.ExtensionContext) => {
 		Commands.registerSetMode()
 	];
 
-	context.subscriptions.push(...commands);
+	context.subscriptions.push(...commands,vscode.languages.registerHoverProvider('html', {
+		provideHover(document, position, token) {
+			const line = position.line
+			const range = document.lineAt(line).range
+			const text = document.getText(range);
+			const translateTest = new RegExp(/{{.*?'([A-Za-z0-9_\\.]+)'.*?\|.*?translate.*?}}/, 'g');
+			const matches = [...text.matchAll(translateTest)];
+			let hoveredMatch;
+			for (const match of matches) {
+				const matchStart = match.index!;
+				const matchEnd = matchStart + match[0].length
+				if (matchStart <= position.character && matchEnd >= position.character) {
+					hoveredMatch = match[1];
+					break;
+				}
+
+			}
+			if (hoveredMatch) {
+				return new vscode.Hover(`${hoveredMatch}`);
+			}
+		}
+	}));
 };
 
 export const deactivate = () => {};

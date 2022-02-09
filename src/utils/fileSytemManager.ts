51aca0876e14c99f90e566e27777db2295a0fd41
Marcin Kuryło <mkurylo@catalogicsoftware.com>
2022-02-09 18:37:32 +0100

    documentation/add-readme

--- src/utils/fileSytemManager.ts
+++ src/utils/fileSytemManager.ts
@@ -13,8 +13,8 @@ export class FileSystemManager {
   public static async fetchJson(): Promise<any> {
     try {
       const uri = await FileSystemManager.getUri();
-      const file = await vscode.workspace.fs.readFile(uri);
-      return JSON.parse(file.toLocaleString());
+      const file = (await vscode.workspace.fs.readFile(uri)).toLocaleString();
+      return file.length ? JSON.parse(file?.toLocaleString()) : {};
     } catch (e) {
       NotificationManager.showErrorMessage(`No file with ${ExtensionConfigManager.getConfigValue('language')} translations found`);
     }
